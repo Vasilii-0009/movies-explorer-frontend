410632d50578c429c6fb049e133f7f96b55a8b5e
Василий Морарь <vasili-morari@MacBook-Air-Vasilij.local>
2023-04-29 00:46:52 +0300

    add-context

--- my-movies/src/components/App/App.js
+++ my-movies/src/components/App/App.js
@@ -1,111 +1,130 @@
-import './App.css';
-import React, { useEffect, useState, } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import Main from '../Main/Main';
-import Footer from '../Footer/Footer';
-import Register from '../Register/Register';
-import Login from '../Login/Login';
-import Layoute from '../Layoute';
-import Preloader from '../Movies/Preloader/Preloader';
-import MoviesCardsList from '../Movies/MoviesCardList/MoviesCardList';
-import SearchForm from '../Movies/SearchForm/SearchForm';
-import SavedMovies from '../SavedMovies/MoviesCardList/MoviesCardList';
-import Profile from '../Profile/Profile';
-import PageNotFound from '../PageNotFound/PageNotFound';
-import Navigation from '../Navigation/Navigation';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
+import Register from "../Register/Register";
+import Login from "../Login/Login";
+import Layoute from "../Layoute";
+import Preloader from "../Movies/Preloader/Preloader";
+import MoviesCardsList from "../Movies/MoviesCardList/MoviesCardList";
+import SearchForm from "../Movies/SearchForm/SearchForm";
+import SavedMovies from "../SavedMovies/MoviesCardList/MoviesCardList";
+import Profile from "../Profile/Profile";
+import PageNotFound from "../PageNotFound/PageNotFound";
+import Navigation from "../Navigation/Navigation";
+import NavigationPopupContext from "../../context/NavigationPopupContext";
+import HidenNavigation from "../../context/HidenNavigation";
 
 function App() {
-   // Route
-   const navigateRegister = useNavigate()
-   const navigateLogin = useNavigate()
+  // Route
+  const navigateRegister = useNavigate();
+  const navigateLogin = useNavigate();
 
-   function handeleRegisterUser() {
-      navigateRegister('/signin')
-   }
+  function handeleRegisterUser() {
+    navigateRegister("/signin");
+  }
 
-   function handeleLogin() {
-      navigateLogin('/movies')
-   }
+  function handeleLogin() {
+    navigateLogin("/movies");
+  }
 
-   // Burger
-   const [isNavigat, setNavigate] = useState('')
-   const [isBurger, setBurger] = useState('')
+  // Burger
+  const [isNavigat, setNavigate] = useState("");
+  const [isBurger, setBurger] = useState("");
 
-   function ShowNavigition() {
-      if (isNavigat === 'navigation-show') {
-         setNavigate('')
-         setBurger('')
-      } else {
-         setNavigate('navigation-show')
-         setBurger('header-movies__burger-active')
-      }
-   }
-   function HidenNanigation() {
-      setNavigate('')
-      setBurger('')
+  function ShowNavigition() {
+    if (isNavigat === "navigation-show") {
+      setNavigate("");
+      setBurger("");
+      document.body.classList.remove("block");
+    } else {
+      setNavigate("navigation-show");
+      setBurger("header-movies__burger-active");
+      document.body.classList.add("block");
+    }
+  }
+  function HidenNanigation() {
+    setNavigate("");
+    setBurger("");
+    document.body.classList.remove("block");
+  }
 
-   }
-
-   return (
-      <div className="App">
-         <Routes>
-            <Route path="/" element={<Layoute isBurger={isBurger} onClick={ShowNavigition} />} >
-
-               <Route path='/' element={
+  return (
+    <div className="App">
+      <NavigationPopupContext.Provider value={isNavigat}>
+        <HidenNavigation.Provider value={HidenNanigation}>
+          <Routes>
+            <Route
+              path="/"
+              element={<Layoute isBurger={isBurger} onClick={ShowNavigition} />}
+            >
+              <Route
+                path="/"
+                element={
                   <>
-                     <Main />
-                     <Footer />
-                     <Navigation isNavigat={isNavigat} onClick={HidenNanigation} />
+                    <Main />
+                    <Footer />
+                    <Navigation />
                   </>
-
-               } />
-               <Route path='/movies' element={
+                }
+              />
+              <Route
+                path="/movies"
+                element={
                   <>
-                     <SearchForm />
-                     <MoviesCardsList />
-                     <Footer />
-                     <Navigation isNavigat={isNavigat} onClick={HidenNanigation} />
-                     {/* <Preloader /> */}
+                    <SearchForm />
+                    <MoviesCardsList />
+                    <Footer />
+                    <Navigation />
+                    {/* <Preloader /> */}
                   </>
-
-               } />
-               <Route path='/saved-movies' element={
+                }
+              />
+              <Route
+                path="/saved-movies"
+                element={
                   <>
-                     <SearchForm />
-                     <SavedMovies />
-                     <Footer />
-                     <Navigation isNavigat={isNavigat} onClick={HidenNanigation} />
+                    <SearchForm />
+                    <SavedMovies />
+                    <Footer />
+                    <Navigation />
                   </>
-               }
-               />
-               <Route path='/profile' element={
+                }
+              />
+              <Route
+                path="/profile"
+                element={
                   <>
-                     <Profile />
-                     <Navigation isNavigat={isNavigat} onClick={HidenNanigation} />
+                    <Profile />
+                    <Navigation />
                   </>
-               }
-               />
+                }
+              />
             </Route>
 
-            <Route path='/signup' element=
-               {
-                  <>
-                     <Register handeleRegisterUser={handeleRegisterUser} />
-                  </>
-               } />
-            <Route path='/signin' element=
-               {
-                  <>
-                     <Login handeleLogin={handeleLogin} />
-                  </>
-               } />
-            <Route path='*' element={
-               <PageNotFound />
-            } />
-
-         </Routes>
-      </div>
-   );
+            <Route
+              path="/signup"
+              element={
+                <>
+                  <Register handeleRegisterUser={handeleRegisterUser} />
+                </>
+              }
+            />
+            <Route
+              path="/signin"
+              element={
+                <>
+                  <Login handeleLogin={handeleLogin} />
+                </>
+              }
+            />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </HidenNavigation.Provider>
+      </NavigationPopupContext.Provider>
+    </div>
+  );
 }
 
 export default App;
--- my-movies/src/components/Navigation/Navigation.js
+++ my-movies/src/components/Navigation/Navigation.js
@@ -1,21 +1,48 @@
-import './Navigation.css'
+import "./Navigation.css";
+import React from "react";
 import { NavLink } from "react-router-dom";
-import Acaunt from '../Acaunt/Acaunt';
+import Acaunt from "../Acaunt/Acaunt";
+import NavigationPopupContext from "../../context/NavigationPopupContext";
+import HidenNavigation from "../../context/HidenNavigation";
 
 function Navigation(props) {
-
-   return (
-      <div className={` navigation  ${props.isNavigat} `}>
-         <div className="navigation__container">
-            <div className="navigation__box-movies">
-               <NavLink onClick={props.onClick} className='link navigation__main' to='/'> <span className="navigation__main-text">Главная</span></NavLink>
-               <NavLink onClick={props.onClick} className='link navigation__movies' to='/movies'> <span className="navigation__movies-text">Фильмы</span></NavLink>
-               <NavLink onClick={props.onClick} className='link navigation__movies-save' to='/saved-movies'> <span className="navigation__movies-save-text">Сохранённые фильмы</span></NavLink>
-            </div>
-            <Acaunt onClick={props.onClick} />
-         </div>
+  const navigationContext = React.useContext(NavigationPopupContext);
+  const hidenNavigatin = React.useContext(HidenNavigation);
+  return (
+    <div className={` navigation  ${navigationContext} `}>
+      <div className="navigation__container">
+        <div className="navigation__box-movies">
+          <NavLink
+            onClick={hidenNavigatin}
+            className="link navigation__main"
+            to="/"
+          >
+            {" "}
+            <span className="navigation__main-text">Главная</span>
+          </NavLink>
+          <NavLink
+            onClick={hidenNavigatin}
+            className="link navigation__movies"
+            to="/movies"
+          >
+            {" "}
+            <span className="navigation__movies-text">Фильмы</span>
+          </NavLink>
+          <NavLink
+            onClick={hidenNavigatin}
+            className="link navigation__movies-save"
+            to="/saved-movies"
+          >
+            {" "}
+            <span className="navigation__movies-save-text">
+              Сохранённые фильмы
+            </span>
+          </NavLink>
+        </div>
+        <Acaunt o onClick={hidenNavigatin} />
       </div>
-   )
+    </div>
+  );
 }
 
 export default Navigation;
